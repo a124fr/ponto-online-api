e7ed5e8565109cc0846f24d546f8599e56c5d110
Francisco de Assis <a124fr@gmail.com>
2024-06-29 17:26:37 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/xyz/pontoonline/PontoOnlineApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.pontoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoOnlineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoOnlineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/pontoonline/PontoOnlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pontoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoOnlineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
